6284521cd73b894666cd207ed83d6fd6a6362234
khasanov_ibroxim <xibroxim11@gmail.com>
2025-12-14 03:16:27 +0500

    asd

--- app/object/[id]/page.tsx
+++ app/object/[id]/page.tsx
@@ -1,4 +1,4 @@
-// This page is now handled by app/[lang]/object/[id]/page.tsx
+// This page is now handled by app/[lang]/object/[id]/page.tsx dew w
 export default function Page() {
   return null;
 }
